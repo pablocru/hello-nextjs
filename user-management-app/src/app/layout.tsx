5e2b5dca5d1ee144d724a838e7fe85ba9144f08f
pabcrudel <pabcrudel@gmail.com>
2024-03-07 19:41:28 +0100

    feat: Initiate User Management App

---
+++ user-management-app/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import { plexSans, plexSerif } from '../../../common/fonts';
+import '../styles/main.css';
+
+export const metadata: Metadata = {
+  title: 'User Management App',
+  description: 'Supabase Database, Auth and Storage'
+};
+
+export default function RootLayout ({
+  children
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${plexSans.variable} ${plexSerif.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
