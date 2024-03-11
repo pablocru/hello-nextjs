9e5e79708490b60e39e50356c65837933152bb10
pabcrudel <pabcrudel@gmail.com>
2024-03-11 21:36:58 +0100

    feat: Handle confirmation code and sign out requests

---
+++ projects/user-management-app/src/app/auth/signout/route.ts
@@ -0,0 +1,23 @@
+// Source: https://supabase.com/docs/guides/getting-started/tutorials/with-nextjs?language=ts#sign-out
+
+import { createClient } from '@/utils/supabase/server';
+import { revalidatePath } from 'next/cache';
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function POST (req: NextRequest) {
+  const supabase = createClient();
+
+  // Check if a user's logged in
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    await supabase.auth.signOut();
+  }
+
+  revalidatePath('/', 'layout');
+  return NextResponse.redirect(new URL('/login', req.url), {
+    status: 302
+  });
+}
